9b5d714c36d4a630cb2d684b1c0b6bfffb35b07f
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 11:15:07 -0700

    added DB seed file

---
+++ utils/seedDB.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const db = require('../config/connection');
+
+const { User, Thought } = require('../models');
+
+mongoose.connect(process.env.MONGODB_URI ||' mongodb://localhost:27017', {
+    //Mongoose, Use Mongo's new URL Parser
+    useNewUrlParser: true,
+    //Mongoose, Use Mongo's new Server Discover/Monitoring Engine
+    useUnifiedTopology: true
+});
+
+//User Seeds 
+const userData = [
+    {
+        username: 'briimcfly',
+        email: 'bickel@hey.com'
+    },
+    {
+        username: 'thatbandlightweight',
+        email: 'lightweightband@gmail.com'
+    }
+];
+
+User.insertMany(userData)
+    .then(insertedUsers => {
+        const thoughtData = [
+            {
+                thoughtText: "I really like that band Lightweight!",
+                username: insertedUsers[0].username,
+                userId: insertedUsers[0]._id,
+                reactions:[
+                    {
+                        reactionBody: "Thank you!",
+                        username: insertedUsers[1].username,
+                        userId: insertedUsers[1]._id
+                    }
+                ]
+            },
+            {
+                thoughtText: 'We have a show on Friday!',
+                username: insertedUsers[1].username,
+                userId: insertedUsers[1]._id
+            }
+        ];
+
+        returnThought.insertMany(thoughtData);
+    })
+    .then(() => {
+        console.log('Data seeded!');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    })
