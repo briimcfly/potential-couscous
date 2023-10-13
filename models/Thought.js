463a93ae98d50b3dd22cff31b76bf7f712bf508b
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 05:19:25 -0700

    created Thought schema

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+//Thought Schema 
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new Date(timestamp).toLocaleString()
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction'
+        }
+    ]
+})
+
+const Thought = mongoose.model('Thoughts', thoughtSchema);
+
+module.exports = Thought;
