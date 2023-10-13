154b4f2efdcfafe24a415724bfab785e77fa518f
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 07:37:18 -0700

    added userId to thought reaction subdoc schema

--- models/Thought.js
+++ models/Thought.js
@@ -17,6 +17,11 @@ const reactionSchema = new Schema({
         type: String,
         required: true,
     },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now,
