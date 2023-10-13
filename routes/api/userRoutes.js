3744c54e6267aef863b9445ccfd04426b775dced
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 06:51:10 -0700

    updated user router

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
-// const {
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
 
-// } = require('../../controllers/userController');
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router 
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
 
 module.exports = router;
