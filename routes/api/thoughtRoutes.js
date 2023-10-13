f826e255106fa1ca08996be365d929797e9e1c17
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 08:04:43 -0700

    added thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
-// const {
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
 
-// } = require('../../controllers/thoughtController');
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/users/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+// /api/users/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/reactionId')
+    .post(addReaction)
+    .delete(deleteReaction)
 
 module.exports = router;
