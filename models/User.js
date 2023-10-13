b33bd7658dc944f96d2fbe0cdea48a260a27e143
Bryan Bickel <briimcfly@gmail.com>
2023-10-13 05:14:10 -0700

    created User schema

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// User Schema 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        //regex for email validation 
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types,ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
